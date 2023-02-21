9b4c6aa141f0d20ee4540efb4257d9a6bb8281a7
Amirkhan567 <amrkh5678@gmail.com>
2023-02-21 11:26:45 +0500

    object practice

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// var student = ["amir","naif","furqan","ali","naveed"]
+// student.includes("amir")
+// alert(student.includes("azaib"))
+
+// using include & indexOf method //
+
+// var fruits = ["apple","orange","banana","pineapple"]
+// var askUser = prompt("which fruits you want to buy")
+// var product = fruits.includes(askUser)
+// fruits.indexOf(askUser)
+// if (product){
+//     alert(`${askUser} is available at index of ${fruits.indexOf(askUser)}`)
+// }else{
+//     alert(`${askUser} is not available at index of ${fruits.indexOf(askUser)}`)
+
+// }
+
+// cricketer
+
+// var cricketer = ["babar azam","fakhar zaman","shadab khan","hassan ali","rizwan","haris rauf",
+// "shaheen afridi","naseem shah"]
+// var askCricketFan = prompt("which is your favrt craricketer in this queue")
+// var cricketerQueue = cricketer.includes(askCricketFan)
+// cricketer.indexOf(askCricketFan)
+
+// if(cricketerQueue){
+//     alert(`your favrt cricketer ${askCricketFan} is at index No. ${cricketer.indexOf(askCricketFan)} in our queue`)
+
+// }else{
+//     alert(`your favrt cricketer ${askCricketFan} is not available in our queue`)
+
+// }
+
+// object
+
+// var person = {
+//     name: "Amir",
+//     eyeCOlor: "greenish",
+//     age: 22,
+//     address: "hawksbay",
+//     skill: ["html","css","bootstap","excel"]
+// };
+// console.log(person.skill[3])
